d97c1bc5ef18bfb03776e74d6b25dfcb73becf0d
Scott Gilmour <starbaster100@gmail.com>
2015-04-18 20:25:54 -0700

    Update wat.js

--- wat.js
+++ wat.js
@@ -4,13 +4,18 @@ if (process.argv.length < 4) {
 }
 
 var question = process.argv[3];
-var fields = 'items(title, displayLink, snippet)';
-var cx = '013972680988251263703:slglaqoes_q';
+var blacklist = ['Stack Overflow'];
+var google = {
+    fields: 'items(title, displayLink, snippet)',
+    cx: '013972680988251263703:slglaqoes_q',
+    api: 'AIzaSyA7iZi70URK1fsqkjyLVFSaiE1iXtRv7tk',
+};
+
+var options = {
+    url: 'https://www.googleapis.com/customsearch/v1?key=' + google.api + '&fields=' + google.fields + '&cx=' + google.cx + '&q="' + question + '"'
+};
 
 var request = require('request');
-var options = {
-    url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyA7iZi70URK1fsqkjyLVFSaiE1iXtRv7tk&fields=' + fields + '&cx=' + cx + '&q="' + question + '"'
-};
 
 function callback(error, response, body) {
     if (response.statusCode != 200) {
@@ -21,10 +26,21 @@ function callback(error, response, body) {
         var results = JSON.parse(body);
         console.log('query returned ' + results.items.length + ' items');
 
+        console.log(removeCommonWords(results.items[0].title));
+
         for (var i = 0; i < results.items.length; i++) {
-            console.log(results.items[i].title + ' - ' + results.items[i].snippet + '\n');
+            console.log(removeCommonWords(results.items[i].title) + ' - ' + results.items[i].snippet);
         }
     }
+}
+
+function removeCommonWords(title) {
+    var content = title;
+    for (var word in blacklist) {
+        content = content.replace(word, 'bobba fett');
+    }
+    console.log(content);
+    return content;
 }
 
 function print_usage() {
